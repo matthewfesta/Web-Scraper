0f5be28936f4cc5fe8a6218ec4a9b01254c24f14
Matthew Festa <festa.matthew@me.com>
2022-09-17 22:17:59 -0400

    added more functionality

--- web_scraper.py
+++ web_scraper.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 
 # Capture the URL as a result variable
 result = requests.get("https://www.google.com/")
-print(result.status_code)  # Make sure page is accessible
-print(result.headers)  # Get headers to verify correct page
+print(f'Status Code: \n {result.status_code}')  # Make sure page is accessible
+print(f'Headers: \n {result.headers}')  # Get headers to verify correct page
+print('\n')
 src = result.content  # Extract the content and store it in a variable
 # Create soup object to parse and process the source
-soup = BeautifulSoup(src, 'lxml')
+soup = BeautifulSoup(src, "html.parser")
+# Find all <a> tags to get the links:
+links = soup.find_all("a")
+print('Links:')
+for link in links:
+	print(link)
+print('\n')
 
