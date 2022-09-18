539dfba0918167b0c5997d2dd703258ce66eef18
Matthew Festa <festa.matthew@me.com>
2022-09-17 23:32:16 -0400

    changed menu structure

--- web_scraper.py
+++ web_scraper.py
@@ -1,23 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-def menu_options():
-	print('Menu Options: \n'
-	      '1 - Get headers'
-	      '2 - Get Document Structure'
-	      '3 - Get Links'
-	      '4 - Get Email Addresses'
-	      '5 - Search by HTML Tag'
-	      '6 - Quit')
-	pass
+
 def get_headers():
 	pass
+
+
 def get_structure():
 	pass
+
+
 def get_links():
 	pass
+
+
 def get_emails():
 	pass
+
+
 def get_tag():
 	pass
 
@@ -30,9 +30,40 @@ try:
 except:
 	print('Invalid URL', url_input)
 else:
+	print(url_input)
+	# Check status code to make sure it is accessible
+	print(f'Status Code: \n {result.status_code}')
 	quit = False
 	while not quit:
-	print(f'Status Code: \n {result.status_code}')  # Make sure page is accessible
+		menu_input = input('Menu Options: \n'
+		                   '1 - Get headers'
+		                   '2 - Get Document Structure'
+		                   '3 - Get Links'
+		                   '4 - Get Email Addresses'
+		                   '5 - Search by HTML Tag'
+		                   '6 - Quit')
+		try:
+			menu_input = int(menu_input)
+			if menu_input < 1 or menu_input > 6:
+				print('Invalid Input')
+		except:
+			print('Invalid Input')
+		else:
+			if menu_input == 1:
+				get_headers()
+			elif menu_input == 2:
+				get_structure()
+			elif menu_input == 3:
+				get_links()
+			elif menu_input == 4:
+				get_emails()
+			elif menu_input == 5:
+				get_tag()
+			elif menu_input == 6:
+				quit = True
+
+
+	)  # Make sure page is accessible
 	# Get headers to verify correct page
 	for key, value in result.headers.items():
 		print(key, ":", value)
@@ -55,7 +86,6 @@ for link in links:
 	if "About" in link.text:
 		print(link)
 		print(link.attrs['href'])
-
 
 # Find all occurrences of a tag:
 tag_input = input('Enter tag: ')
