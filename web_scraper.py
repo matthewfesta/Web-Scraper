85b74e0b9e0221e5837e6dd3e01991831c2203eb
Matthew Festa <festa.matthew@me.com>
2022-09-17 21:58:07 -0400

    Extracted content and store it in variable

--- web_scraper.py
+++ web_scraper.py
@@ -4,5 +4,6 @@ from bs4 import BeautifulSoup
 # Capture the URL as a result variable
 result = requests.get("https://www.google.com/")
 print(result.status_code)  # Make sure page is accessible
-print(result.headers)  # Get headers
+print(result.headers)  # Get headers to verify correct page
+src = result.content  # Extract the content and store it in a variable
 
