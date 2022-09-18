6791800a642c90fe76636dd46d749abc30c1926c
Matthew Festa <festa.matthew@me.com>
2022-09-17 21:54:33 -0400

    added result and print statement to get the url request

---
+++ web_scraper.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Capture the URL as a result variable
+result = requests.get("https://www.google.com/")
+print(result.status_code)  # Make sure page is accessible
