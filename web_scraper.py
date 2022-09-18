b9ea5bdb3aeb9d79befc1d1ce0b8f76ecb71220f
Matthew Festa <festa.matthew@me.com>
2022-09-17 23:13:13 -0400

    starting to add user functionality

--- web_scraper.py
+++ web_scraper.py
@@ -2,7 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 # Capture the URL as a result variable
-result = requests.get("https://www.whitehouse.gov/briefings-statements/")
+
+url_input = input('Enter URL: \n')
+try:
+	result = requests.get(url_input)
+except:
+	print('Invalid URL', url_input)
+
 print(f'Status Code: \n {result.status_code}')  # Make sure page is accessible
 # Get headers to verify correct page
 for key, value in result.headers.items():
@@ -11,6 +17,8 @@ print('\n')
 src = result.content  # Extract the content and store it in a variable
 # Create soup object to parse and process the source
 soup = BeautifulSoup(src, "html.parser")
+print(f'Page Structure: \n {soup.prettify()}')
+print('\n')
 # Find all <a> tags to get the links:
 links = soup.find_all("a")
 # Print all links:
@@ -26,3 +34,10 @@ for link in links:
 		print(link.attrs['href'])
 
 
+# Find all occurrences of a tag:
+tag_input = input('Enter tag: ')
+print(soup.find_all(tag_input))
+
+# Find first occurrence of a tag:
+tag_input = input('Enter tag: ')
+print(soup.tag_input)
