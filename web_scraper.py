34b357f2fe3a58154a1edef6c24cb37e8903ca11
Matthew Festa <festa.matthew@me.com>
2022-09-17 21:55:38 -0400

    get headers

--- web_scraper.py
+++ web_scraper.py
@@ -4,3 +4,5 @@ from bs4 import BeautifulSoup
 # Capture the URL as a result variable
 result = requests.get("https://www.google.com/")
 print(result.status_code)  # Make sure page is accessible
+print(result.headers)  # Get headers
+
