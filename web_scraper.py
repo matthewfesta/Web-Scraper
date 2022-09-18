a25b69318fdf490640299b2536d6906a27ddfe52
Matthew Festa <festa.matthew@me.com>
2022-09-17 23:53:08 -0400

    added emails

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -24,6 +25,11 @@ def get_structure(url_result):
 
 
 def get_links(url_result):
+	"""
+	Retrieves the links on the page
+	:param url_result:
+	:return:
+	"""
 	src = url_result.content  # Extract the content and store it in a variable
 	# Create soup object to parse and process the source
 	soup = BeautifulSoup(src, "html.parser")
@@ -38,8 +44,19 @@ def get_links(url_result):
 
 
 def get_emails(url_result):
+	"""
+	Retrieves tne emails on the page
+	:param url_result:
+	:return:
+	"""
 	EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-
+	# for Javascript rendered websites:
+	url_result.html.render()
+	for re_match in re.finditer(EMAIL_REGEX, url_result.html.raw_html.decode()):
+		try:
+			print(re_match.group())
+		except:
+			print('Sorry, I can\'t find any emails')
 
 
 def get_tag(url_result):
